98c3c3104412c332f2aee545ac68546caabb415b
GertSallaerts <1267900+GertSallaerts@users.noreply.github.com>
2024-06-26 20:04:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+
+const file = process.argv[2];
+const contents = fs.readFileSync(file, 'utf8');
+const lines = contents.split('\n').filter(Boolean);
+
+const MAX_CHARGE = 5
+
+const COLUMNS = {
+    type: 7,
+    amount: 8,
+};
+
+const BATTERY = {
+    max: MAX_CHARGE,
+    current: 0,
+    usage: 0,
+};
+
+const NET = {
+    inject: 0,
+    usage: 0,
+}
+
+lines.shift();
+
+for (const line of lines) {
+    const parts = line.split(';');
+
+    const type = parts[COLUMNS.type];
+    const amountStr = parts[COLUMNS.amount];
+    const amount = parseFloat(amountStr.replace(/\./g, '').replace(',', '.'));
+
+    console.log(amount, type)
+
+    if (type.startsWith('Injectie')) {
+        const charge = Math.min(BATTERY.max - BATTERY.current, amount);
+        const inject = amount - charge;
+
+        BATTERY.current += charge;
+        NET.inject += inject;
+    } else {
+        const battery = Math.min(BATTERY.current, amount);
+        const net = amount - battery;
+
+        BATTERY.current -= battery;
+        BATTERY.usage += battery;
+        NET.usage += net;
+    }
+}
+
+console.log({
+    BATTERY,
+    NET
+})
+
+
